5cefe63d765dceb6c85670917d0a81be71f148fa
Odis Clemons <odis.clemons@gmail.com>
2022-07-31 13:08:51 -0400

    added FIT login screen

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import background from '../../images/login_bg.jpg'
+import React from "react";
+import bkgImage from "../../images/login_bg.jpg";
 
 const Home = () => {
-    const backGround = {
-        backgroundImage:`url(${background})`,
-        backgroundSize:"cover",
-        backgroundRepeat:"no-repeat",
-        height:'100vh'
-        // background-position: "static"
-    }
+  const bkgStyle = {
+    backgroundImage: `url(${bkgImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    // background-position: "static"
+  };
   return (
+    <div className="pageContainer" style={bkgStyle}>
+      <div className="loginContainer">
+        <div className="loginContainerLeft">
+          <div>
+            <img src="images/FITlogo.png" />
+            <p>Welcome to F.I.T.</p>
+          </div>
 
-    <div className='image' style={backGround} >
-      
+          <p>Login to find the perfect workout partner.</p>
+          <input type="text" placeholder="email address" />
+          <input type="password" placeholder="password" />
+
+          <button></button>
+        </div>
+
+        <div className="loginContainerRight">
+          <div class="text-white px-3 py-4 ">
+            <h2 class="mb-0">Friends In Fitness</h2>
+            <h5 class="mb-4">More than just gym partners.</h5>
+            <p class="mb-4">
+              Find, match, and train with a unique mix of local gym goers
+              ranging from novice to enthusiast. <br /> Find your fit, with
+              F.I.T.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
